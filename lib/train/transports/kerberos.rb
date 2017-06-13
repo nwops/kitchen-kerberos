fea641fcb7bca5896f43f22077183a623673b028
Corey Osman <corey@nwops.io>
2017-06-13 16:54:43 -0700

    Adds train transport
      * This adds support to the train gem by adding a new transport of
        kerberos ssh authentication.  This is really just a subclass of
        ssh which forces auth_methods to be gssapi

---
+++ lib/train/transports/kerberos.rb
@@ -0,0 +1,84 @@
+# encoding: utf-8
+#
+# Author:: Fletcher Nichol (<fnichol@nichol.ca>)
+# Author:: Dominik Richter (<dominik.richter@gmail.com>)
+# Author:: Christoph Hartmann (<chris@lollyrock.com>)
+#
+# Copyright (C) 2014, Fletcher Nichol
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'net/ssh'
+require 'net/scp'
+require 'train/errors'
+require 'train/transports/ssh'
+require 'net/ssh/kerberos'
+module Train::Transports
+  # Wrapped exception for any internally raised SSH-related errors.
+  #
+  # @author Fletcher Nichol <fnichol@nichol.ca>
+  class SSHFailed < Train::TransportError; end
+  class SSHPTYFailed < Train::TransportError; end
+
+  # A Transport which uses the SSH protocol to execute commands and transfer
+  # files.
+  #
+  # @author Fletcher Nichol <fnichol@nichol.ca>
+  class Kerberos < Train::Transports::Ssh
+    name 'kerberos'
+
+    private
+
+    def validate_options(options)
+      super(options)
+      if options[:pty]
+        logger.warn('[SSH] PTY requested: stderr will be merged into stdout')
+      end
+
+      super
+      self
+    end
+
+    # Builds the hash of options needed by the Connection object on
+    # construction.
+    #
+    # @param opts [Hash] merged configuration and mutable state data
+    # @return [Hash] hash of connection options
+    # @api private
+    def connection_options(opts)
+      {
+        logger:                 logger,
+        user_known_hosts_file:  '/dev/null',
+        paranoid:               false,
+        hostname:               opts[:host],
+        port:                   opts[:port],
+        username:               opts[:user],
+        compression:            opts[:compression],
+        compression_level:      opts[:compression_level],
+        keepalive:              opts[:keepalive],
+        keepalive_interval:     opts[:keepalive_interval],
+        timeout:                opts[:connection_timeout],
+        connection_retries:     opts[:connection_retries],
+        connection_retry_sleep: opts[:connection_retry_sleep],
+        max_wait_until_ready:   opts[:max_wait_until_ready],
+        auth_methods:           %w[gssapi-with-mic],
+        keys_only:              false,
+        keys:                   opts[:key_files],
+        password:               opts[:password],
+        forward_agent:          opts[:forward_agent],
+        transport_options:      opts,
+      }
+    end
+
+  end
+end
